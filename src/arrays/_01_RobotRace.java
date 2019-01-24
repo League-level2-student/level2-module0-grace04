36b1773583ab51f743767476b37d2c3b68d12e80
grace04 <36498022+grace04@users.noreply.github.com>
2019-01-23 20:06:17 -0800

    Add files via upload

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -7,70 +7,99 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	// 1. make a main method
+	//1. make a main method
+	static Robot[] bots = new Robot[5];	
+	static int n;
+	static int u;
+	static int d;
+	static int t;
+	static int q;
+	
 	public static void main(String[] args) {
+		
+	//2. create an array of 5 robots.
 		_01_RobotRace rr = new _01_RobotRace();
-		rr.line();
-		// 2. create an array of 5 robots.
-		Robot[] bots = new Robot[5];
-
-		int i = 0;
-		// 3. use a for loop to initialize the robots.
-		for (i = 0; i < 5; i++) {
+		
+	//3. use a for loop to initialize the robots.
+		for (int i=0; i<5; i++) {
 			bots[i] = new Robot();
 			bots[i].miniaturize();
 			bots[i].setSpeed(10);
-			bots[i].moveTo(100 * i + 100, 500);
-		}
-
-		for (i = 0; i < 5; i++) {
-			Random ran = new Random();
-			int dom = ran.nextInt(256);
-			bots[i].setPenColor(dom, dom, dom);
-			bots[i].setPenWidth(8);
+			bots[i].moveTo(100*i+100, 500);
 			bots[i].penDown();
 		}
-		// 4. make each robot start at the bottom of the screen, side by side, facing up
-
-		// 5. use another for loop to iterate through the array and make each robot move
-		// a random amount less than 50.
-		for (int y = 500; y > 50; y--) {
-			for (int x = 0; x < 5; x++) {
+		//4. make each robot start at the bottom of the screen, side by side, facing up
+		rr.color();
+		rr.judge();
+	//5. use another for loop to iterate through the array and make each robot move 
+	//   a random amount less than 50.
+		for (int y=500; y>0; y--) {
+			for (int x=0; x<5; x++) {
 				Random gen = new Random();
-				int er = gen.nextInt(50) + 1;
+				int er = gen.nextInt(50)+1;
 				bots[x].move(er);
-
-				int n = bots[0].getY();
-				int u = bots[1].getY();
-				int d = bots[2].getY();
-				int t = bots[3].getY();
-				int q = bots[4].getY();
-
-				if (n < 25 || u < 25 || d < 25 || t < 25 || q < 25) {
-					JOptionPane.showMessageDialog(null, "Robot has won!");
-					break;
-
-				}
+				n = bots[0].getY();
+				u = bots[1].getY();
+				d = bots[2].getY();
+				t = bots[3].getY();
+				q = bots[4].getY();
+				
+				rr.condi();
 			}
-
 		}
+	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
-		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
-		// screen.
+	//7. declare that robot the winner and throw it a party!
 
-		// 7. declare that robot the winner and throw it a party!
+	//8. try different races with different amounts of robots.
 
+	//9. make the robots race around a circular track.
 	}
-
-	public void line() {
-		Robot jud = new Robot();
-		jud.miniaturize();
-		jud.setSpeed(50);
-		jud.moveTo(50, 25);
-		jud.turn(90);
-		jud.setPenColor(255, 0, 0);
-		jud.penDown();
-		jud.move(500);
+	
+	public void judge() {
+		Robot Rob = new Robot();
+		Rob.setSpeed(100);
+		Rob.miniaturize();
+		Rob.moveTo(50, 50);
+		Rob.turn(90);
+		Rob.setPenColor(120, 180, 150);
+		Rob.penDown();
+		Rob.move(500);
 	}
-
+	
+	public void condi() {
+		if (n<=50) {
+			JOptionPane.showMessageDialog(null, "N has won!");
+			System.exit(0);
+		}
+		else if (u<=50) {
+			JOptionPane.showMessageDialog(null, "U has won!");
+			System.exit(0);
+		}
+		else if (d<=50) {
+			JOptionPane.showMessageDialog(null, "D has won!");
+			System.exit(0);
+		}
+		else if (t<=50) {
+			JOptionPane.showMessageDialog(null, "T has won!");
+			System.exit(0);
+		}
+		else if (q<=50) {
+			JOptionPane.showMessageDialog(null, "Q has won!");
+			System.exit(0);
+		}
+	}
+	
+	public void color() {
+		Random ran = new Random();
+		int x;
+		int y;
+		int z;
+		for (int i=0;i<5;i++) {
+			x = ran.nextInt(255)+1;
+			y = ran.nextInt(255)+1;
+			z = ran.nextInt(255)+1;
+			bots[i].setPenColor(x, y, z);
+		}
+	}
 }
